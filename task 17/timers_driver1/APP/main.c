623914bf8ce1d31d9f61fe04f503bf6e1149c8e7
emanabdelhamid241 <emanabdelhami126@gmail.com>
2023-08-17 21:30:21 +0200

    updating timer0 task

--- task 17/timers_driver1/APP/main.c
+++ task 17/timers_driver1/APP/main.c
@@ -16,8 +16,8 @@ void APP_void_tog_led();
 int main(){
     DIO_voidInit();
     LCD_enum_INIT();
-		    DIO_u8_Set_Pin_Direction(DIO_u8_PORTB,DIO_u8_PIN0,DIO_u8_INTIAL_OUTPUT);
-		    DIO_u8_Set_Pin_Value(DIO_u8_PORTB,DIO_u8_PIN0,DIO_u8_LOW);
+		    DIO_u8_Set_Pin_Direction(DIO_u8_PORTA,DIO_u8_PIN0,DIO_u8_INTIAL_OUTPUT);
+		    DIO_u8_Set_Pin_Value(DIO_u8_PORTA,DIO_u8_PIN0,DIO_u8_LOW);
 
 	        GI_enum_GI_Enable();
 	        TIMERS_enum_TIMER0_CTC_SET_CALL_BACK(&APP_void_tog_led);
@@ -33,14 +33,14 @@ return 0;
 void APP_void_tog_led(){
 u8 switcha;
 static u8 counter=0;
-DIO_u8_Set_Pin_Direction(DIO_u8_PORTA,DIO_u8_PIN7,DIO_u8_INPUT);
+DIO_u8_Set_Pin_Direction(DIO_u8_PORTB,DIO_u8_PIN0,DIO_u8_INPUT);
 while(1){
-	DIO_u8_Get_Pin_Value(DIO_u8_PORTA,DIO_u8_PIN7,&switcha);
+	DIO_u8_Get_Pin_Value(DIO_u8_PORTB,DIO_u8_PIN0,&switcha);
     _delay_ms(200);
 
 	if(switcha== DIO_u8_HIGH){
 //TURN ON
-			DIO_u8_Set_Pin_Value(DIO_u8_PORTB,DIO_u8_PIN0,DIO_u8_HIGH);
+			DIO_u8_Set_Pin_Value(DIO_u8_PORTA,DIO_u8_PIN0,DIO_u8_HIGH);
 			counter++;
 			LCD_enum_GO_TO_XY(2,0);
 			LCD_enum_Send_Num(counter);
@@ -48,7 +48,7 @@ while(1){
 		}
 		else{
 //TURN OFF
-			DIO_u8_Set_Pin_Value(DIO_u8_PORTB,DIO_u8_PIN0,DIO_u8_LOW);
+			DIO_u8_Set_Pin_Value(DIO_u8_PORTA,DIO_u8_PIN0,DIO_u8_LOW);
 		}
 }
 }
